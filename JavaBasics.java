b42c235ab67135c57f7e8118a8d8fa7a3445a411
Hiteshj1693 <Hiteshjethava161611@gmail.com>
2024-05-23 00:30:12 +0530

    Pattern

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class JavaBasics {
+    public static void numberPyramid(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=n-i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("N : ");
+        n = sc.nextInt();
+        System.out.println();
+        numberPyramid(n);
     }
 }
